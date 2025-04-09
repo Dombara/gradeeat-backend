9fd65fe3e1c6fa0133b07bb9ba6c911990e94dfd
Yash Patil <stud22.ysp1@gec.ac.in>
2025-04-09 20:47:36 +0530

    render

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
 import os
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 
 MONGO_URI="mongodb+srv://yash:yash@cluster0.bcuflio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -19,6 +19,75 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     return "Welcome to the Flask MongoDB API!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    category_data = [
+        {
+            "id": 1,
+            "icon": "/images/icon/icon-01.svg",
+            "title": "Crafted for SaaS",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        },
+        {
+            "id": 2,
+            "icon": "/images/icon/icon-02.svg",
+            "title": "High-quality Design",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        },
+        {
+            "id": 3,
+            "icon": "/images/icon/icon-03.svg",
+            "title": "Next.js 13 + TypeScript",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        },
+        {
+            "id": 4,
+            "icon": "/images/icon/icon-04.svg",
+            "title": "Sanity Blog and Docs",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        },
+        {
+            "id": 5,
+            "icon": "/images/icon/icon-05.svg",
+            "title": "DB, Auth and Stripe",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        },
+        {
+            "id": 6,
+            "icon": "/images/icon/icon-06.svg",
+            "title": "Regular Free Updates",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        }
+    ]
+    return jsonify(category_data)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

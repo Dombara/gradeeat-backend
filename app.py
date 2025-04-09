abb4f0b3ca94528346221ca885ca59e16f54ae28
Yash Patil <stud22.ysp1@gec.ac.in>
2025-04-10 01:20:47 +0530

    b

--- app.py
+++ app.py
@@ -95,31 +95,31 @@ def get_category_by_id(id):
             "title": "Crafted for SaaS",
             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
         }
-        ,
-        {
-            "id": 2,
-            "icon": "/images/icon/icon-02.svg",
-            "title": "High-quality Design",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
-        },
-        {
-            "id": 3,
-            "icon": "/images/icon/icon-03.svg",
-            "title": "Next.js 13 + TypeScript",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
-        },
-        {
-            "id": 4,
-            "icon": "/images/icon/icon-04.svg",
-            "title": "Sanity Blog and Docs",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
-        },
-        {
-            "id": 5,
-            "icon": "/images/icon/icon-05.svg",
-            "title": "DB, Auth and Stripe",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
-        }
+        # ,
+        # {
+        #     "id": 2,
+        #     "icon": "/images/icon/icon-02.svg",
+        #     "title": "High-quality Design",
+        #     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        # },
+        # {
+        #     "id": 3,
+        #     "icon": "/images/icon/icon-03.svg",
+        #     "title": "Next.js 13 + TypeScript",
+        #     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        # },
+        # {
+        #     "id": 4,
+        #     "icon": "/images/icon/icon-04.svg",
+        #     "title": "Sanity Blog and Docs",
+        #     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        # },
+        # {
+        #     "id": 5,
+        #     "icon": "/images/icon/icon-05.svg",
+        #     "title": "DB, Auth and Stripe",
+        #     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor."
+        # }
         # ,
         # {
         #     "id": 6,
